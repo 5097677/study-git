2b80a8f1a73c63742d4e3232613930de757e2f38
zhangchao <5097677@qq.com>
2020-01-14 15:05:19 +0800

    dev edit b.php

--- b.php
+++ b.php
@@ -2,26 +2,6 @@
 include("./Snoopy.class.php");
 date_default_timezone_set('Asia/Shanghai');
 
-echo 'tgestasdfsddddddddd';
 
-$a = 35;
-if($a > 3){
-   echo '大于';
-}
+echo 'dev 分支删除所有';
 
-echo '测试dev分支修改';
-
-echo '主干上添加内容';
-
-echo '乱沃尔夫就';
-
-
-echo '2020年1月14日新lianxi';
-
-
-echo '分支上修改';
-
-
-echo 'dev 添加代码';
-
-echo 'test';
