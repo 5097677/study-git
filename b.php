b8deb642c767781e051f0088aeabbd661f90020f
zhangchao <5097677@qq.com>
2020-01-14 16:45:49 +0800

    再次删除dev内容

--- b.php
+++ b.php
@@ -2,21 +2,5 @@
 include("./Snoopy.class.php");
 date_default_timezone_set('Asia/Shanghai');
 
+echo '再次删除dev所有';
 
-echo 'dev 分支删除所有';
-
-
-echo 'dev 二次修改';
-
-
-echo 'master 上的修改';
-
-
-echo 'master 尝试冲突';
-
-echo 'hehe';
-
-echo '这里是也不直达欧莎情况';
-echo '远程DEV';
-
-echo '远程第二次提交';
