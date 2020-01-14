dca289b5651a986cf8130a81b7ebc4954f2b582d
zhangchao <5097677@qq.com>
2020-01-14 14:21:48 +0800

    test b.php

--- b.php
+++ b.php
@@ -1,16 +1,8 @@
 ﻿<?php
 include("./Snoopy.class.php");
 date_default_timezone_set('Asia/Shanghai');
-echo '<pre>';
-set_time_limit(0);
 
-echo 'test git';
-echo 'test git 2222';
-echo '这里是测试第三次提交';
-echo '测试两次ADD，只有一次commit';
-
-echo 'hhahahahahha';
-
+echo 'tgestasdfsddddddddd';
 
 echo '测试dev分支修改';
 
